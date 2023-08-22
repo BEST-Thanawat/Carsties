c7cc8fa5abc92bd11557c51fe2d4de4ac2c16cef
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2023-08-21 23:00:43 -0700

    End of section 7

---
+++ AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,34 @@
+﻿using AuctionService.Data;
+using AuctionService.Entities;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers
+{
+    public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+    {
+        private readonly AuctionDbContext dbContext;
+
+        public AuctionFinishedConsumer(AuctionDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task Consume(ConsumeContext<AuctionFinished> context)
+        {
+            Console.WriteLine("--> Consuming bid finished");
+
+            var auction = await dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+            if (context.Message.ItemSold)
+            {
+                auction.Winner = context.Message.Winner;
+                auction.SoldAmount = context.Message.Amount;
+            }
+
+            auction.Status = auction.SoldAmount > auction.ReservePrice ? Status.Finished : Status.ReserveNotMet;
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ SearchService/Program.cs
@@ -0,0 +1,70 @@
+using MassTransit;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Consumers;
+using SearchService.Data;
+using SearchService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Add AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Add Polly
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+// Add RabbitMQ
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+
+        cfg.ReceiveEndpoint("search-auction-created", e =>
+        {
+            e.UseMessageRetry(r => r.Interval(5, 5));
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Add DbInitializer
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
+
+app.Run();
+
+// Add Polly Policy
+static IAsyncPolicy<HttpResponseMessage> GetPolicy() => HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+    .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
